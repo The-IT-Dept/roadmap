0c6fafee093e93ccaad03a7a86dea9a4143883ef
nicko170 <nick@npratley.net>
2024-05-26 16:58:42 +1000

    Fix: disable user registration email

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -27,7 +27,7 @@ class EventServiceProvider extends ServiceProvider
      */
     protected $listen = [
         Registered::class => [
-            SendEmailVerificationNotification::class,
+            //SendEmailVerificationNotification::class,
         ],
     ];
 
